076c8c16d5a6221147a734dc3e1a776206fef24c
Mitskevich Igor <jimaltair1987@gmail.com>
2022-02-01 19:41:46 +0200

    update the application.properties

--- src/main/java/ru/jimaltair/currencyconverter/controller/CurrencyController.java
+++ src/main/java/ru/jimaltair/currencyconverter/controller/CurrencyController.java
@@ -72,7 +72,7 @@ public class CurrencyController {
         // получаем историю конвертаций валютной пары за указанную дату
         List<Exchange> exchangeHistory = conversionService.getHistory(historyForm.getFirstCurrency(),
                 historyForm.getSecondCurrency(), historyForm.getDate());
-        log.info("Received the history with {} records", exchangeHistory.size());
+        log.info("Received the history with {} record(s)", exchangeHistory.size());
         // получаем статистику валютной пары за неделю
         Statistic weekStatistic = conversionService.getStatistic(historyForm.getFirstCurrency(),
                 historyForm.getSecondCurrency(), historyForm.getDate().minusDays(6), historyForm.getDate());
