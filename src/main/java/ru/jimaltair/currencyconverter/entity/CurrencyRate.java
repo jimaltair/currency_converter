92294ccdc2364bb1c71d42fd7a294bc22052af4b
Mitskevich Igor <jimaltair1987@gmail.com>
2022-02-11 17:00:05 +0200

    slight refactoring

--- src/main/java/ru/jimaltair/currencyconverter/entity/CurrencyRate.java
+++ src/main/java/ru/jimaltair/currencyconverter/entity/CurrencyRate.java
@@ -16,7 +16,7 @@ public class CurrencyRate {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(nullable = false)
     private double rate;
--- src/main/java/ru/jimaltair/currencyconverter/entity/Exchange.java
+++ src/main/java/ru/jimaltair/currencyconverter/entity/Exchange.java
@@ -15,7 +15,7 @@ public class Exchange {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "first_currency_code")
